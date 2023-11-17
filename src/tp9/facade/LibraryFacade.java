85ded39b20ddd5b4743d23c49fb2e078b26b7da5
Mickael DP <m.dallepasqualine@gmail.com>
2023-11-17 12:12:09 +0100

    added LibraryFacade

--- src/tp9/facade/LibraryFacade.java
+++ src/tp9/facade/LibraryFacade.java
@@ -11,23 +11,40 @@ import tp9.decorator.LibraryBookDecorator;
 public class LibraryFacade {
     private OldBookStore oldBookStore;
     private IBookStore bookStoreAdapter;
-    // autres attributs si nécessaire
+    private LibraryBookDecorator libraryBookDecorator;
+
 
     public LibraryFacade(OldBookStore oldBookStore) {
-        //TODO à compléter
+        this.oldBookStore = oldBookStore;
+        this.bookStoreAdapter = new BookStoreAdapter(oldBookStore);
+        this.libraryBookDecorator = new LibraryBookDecorator(new Book("Refactoring Guru Code", 14));
     }
 
 
     // Méthodes simplifiées pour l'accès client
     public IBook getBook(String title) {
-        // Logique pour obtenir un aperçu du livre
-        //TODO à compléter
-        return null ;
+        // Logique pour obtenir un aperçu du livre dans l'ancien système
+        IBook book = oldBookStore.getBook(title);
+
+        if (book == null) {
+            // Logique pour obtenir un aperçu du livre dans le nouveau système
+            book = bookStoreAdapter.fetchBook(title);
+        }
+        return book ;
     }
 
     // Méthode qui renvoie un libraryBookDecorator
     public IBook libraryBook(String title) {
-        //TODO à compléter
-        return null;
+        // Obtenez le livre en fonction du titre (à partir de OldBookStore ou BookStoreAdapter)
+        // Utilisons bookStoreAdapter pour obtenir le livre
+        Book book = bookStoreAdapter.fetchBook(title);
+
+        if (book != null) {
+            // Si le livre est trouvé, créez un LibraryBookDecorator avec ce livre
+            return new LibraryBookDecorator(book);
+        } else {
+            // Si le livre n'est pas trouvé, renvoyez null ou gérez le cas en conséquence
+            return null;
+        }
     }
 }
