ca2146f1982705c492c6859e7da2d5409a0e3f03
Nicolas-B06 <nicolas.bernard@nextsourcia.com>
2023-11-17 14:35:33 +0100

    patern proxy

--- src/tp9/Book.java
+++ src/tp9/Book.java
@@ -8,14 +8,31 @@ public class Book implements IBook {
     public Book (String title, double price)
     {
         //TODO à compléter
+        this.title = title;
+        this.price = price;
     }
     // getters, setters, etc.
+    public String getTitle() {
+        return title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 
 
     //TODO à compléter
 
     public String preview() {
         // Simule l'affichage d'un aperçu du livre (indiquez par exemple "Preview of Clean Code: [First few lines of the book...]")
-        return null;
+        return "Preview of " + title + ": Priced at " + price;
     }
 }
